9c1c2bf848afcc883617c40b4c8b5eb0ac16ebf9
Joey <jtroinacoding@gmail.com>
2022-01-03 22:20:44 -0500

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by admin on 1/3/22.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
